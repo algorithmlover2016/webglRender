229bfb897ef628d0419255f25bfee20aa85b4003
bo yu <boyu@bos-MacBook-Pro.local>
2023-06-30 12:30:52 +0800

    add textDemo with tsc build command add build command

---
+++ textDemo/build.sh
@@ -0,0 +1,20 @@
+# sudo npm install -g npm@9.7.2
+
+# to install typescript
+npm install -g typescript
+
+
+# to create package.json
+npm init
+
+# to create tsconfig.json
+tsc --init
+
+# to build project
+npm run build
+
+# to run project
+npm run dev
+
+# to install webpack
+sudo npm install --save-dev webpack webpack-cli ts-loader
---
+++ textDemo/src/func.ts
@@ -0,0 +1,18 @@
+function reverse(s: string): string {
+    return s.split("").reverse().join("");
+}
+
+function revbtn(): void {
+    const in1: HTMLInputElement = document.getElementById("in1") as HTMLInputElement;
+    const ou1: HTMLDivElement = document.getElementById("ou1") as HTMLDivElement;
+    const s: string = in1.value;
+    ou1.innerText = reverse(s);
+}
+
+function clrbtn(): void {
+    const in1: HTMLInputElement = document.getElementById("in1") as HTMLInputElement;
+    const ou1: HTMLDivElement = document.getElementById("ou1") as HTMLDivElement;
+    in1.value = "";
+    in1.innerText = "";
+    ou1.innerText = "";
+}
